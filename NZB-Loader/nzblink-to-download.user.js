8f97c7aaa15e347cf185e618c45326bade9904d9
lordbex <lordibex@protonmail.com>
2023-10-14 19:08:26 +0200

    Updated NZB-Loader to v0.2 / Improved trigger check and event handling.
    Added MutationObserver to handle dynamic elements improving responsiveness.

--- NZB-Loader/nzblink-to-download.user.js
+++ NZB-Loader/nzblink-to-download.user.js
@@ -3,7 +3,7 @@
 // @description         Automatically downloads NZB files from nzbindex.nl when links with the "nzblnk:" scheme are clicked.
 // @description:de_DE   Lädt NZB-Dateien automatisch von nzbindex.nl herunter, wenn auf Links mit dem Schema "nzblnk:" geklickt wird.
 // @author              LordBex
-// @version             0.1
+// @version             0.2
 // @match               *://*/*
 // @grant               GM_xmlhttpRequest
 // ==/UserScript==
@@ -49,32 +49,62 @@ function loadNzb(nzblnk) {
     let nzb_info = parseNzblnkUrl(nzblnk)
     let url = `https://nzbindex.nl/search/json?q=${nzb_info.h}&max=5&minage=0&maxage=0&hidespam=1&hidepassword=0&sort=agedesc&minsize=0&maxsize=0&complete=0&hidecross=0&hasNFO=0&poster=&p=0`
     console.log("NZB geladen");
-    
+
     GM_xmlhttpRequest({
         method: "GET",
         url: url,
         onload: function(response) {
-            let parser = new DOMParser();
             console.log(response)
             let data = JSON.parse(response.responseText);
-            if ( data.total ==  0 ){
+            if ( data.total ===  0 ){
                 alert('NZB nicht gefunden');
                 return
-            } 
+            }
             downloadFile("https://nzbindex.nl/download/" + data.results[0].id, `${nzb_info.t}{{${nzb_info.p}}}`)
         }
     });
 }
 
-function setLNKTrigger(){
-        document.querySelectorAll('a[href^="nzblnk"]').forEach((element) => {
+function setLNKTrigger(element){
+    if (element.tagName === 'A'){
+        if (!element.href.startsWith('nzblnk')){
+            console.log("Kein NZB-LNK element")
+            console.log(element)
+            return
+        }
+        console.log("Update NZB-LNK element")
         element.dataset.link = element.href
         element.href = ""
-        
+
         element.addEventListener('click', (event) => {
-            event.preventDefault();  
-            loadNzb(element.dataset.link);  
+            event.preventDefault();
+            loadNzb(element.dataset.link);
         });
-    });
+    } else {
+        element.querySelectorAll('a[href^="nzblnk"]').forEach((e) => {
+            setLNKTrigger(e)
+        });
+    }
 }
-setLNKTrigger();
+
+function observeSiteChanges(){
+    const callback = function(mutationsList, observer) {
+        for ( let mutation of mutationsList ) {
+            if ( mutation.type === 'childList' ) {
+                for ( let node of mutation.addedNodes ){
+                    if (!node instanceof Node || node.nodeType !== Node.ELEMENT_NODE) {
+                        continue
+                    }
+                    setLNKTrigger(node)
+                }
+            }
+        }
+    };
+    const observer = new MutationObserver(callback);
+    const config = { childList: true, subtree: true };
+    // Die Überwachung starten
+    observer.observe(document, config);
+}
+
+setLNKTrigger(document.body);
+observeSiteChanges();
